688f1717278875fbcfef8072d901cff144124220
Jose Galvez <jjgalper@gmail.com>
2019-07-18 16:35:18 -0500

    Front ready

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    //This could be a functional component, doesn't have to be a class
+    // componentWillUpdate(){
+    //     console.log('[Order Summary] WillUpdate');
+    // }
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
                     <span style={{textTransform: 'capitalize'}}>
-                        {igKey}: {props.ingredients[igKey]}
+                        {igKey}: {this.props.ingredients[igKey]}
                     </span>
                 </li>
             )
         })
-    return (
-        <Aux>
-            <h3>
-                Your order
-            </h3>
-            <p>
-                A delicious burger with the following ingredients:
-            </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>
-                Continue to checkout?
-            </p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    )
-}
 
-export default orderSummary;
+        return(
+            <Aux>
+                <h3>
+                    Your order
+                </h3>
+                <p>
+                    A delicious burger with the following ingredients:
+                </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>
+                    Continue to checkout?
+                </p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        )
+    }
+};
+
+export default OrderSummary;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux'
 
 import './SideDrawer.css'
 
 const sideDrawer = (props) => {
+    let attachedClasses = ['SideDrawer', 'Close'];
+    if(props.open){
+        attachedClasses = ['SideDrawer', 'Open'];
+    }
 
     return(
-        <div className='SideDrawer'>
-            <div className='LogoSide'>
-                <Logo />
-            </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.close}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className='LogoSide'>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>    
+        </Aux>   
+        
     );
 };
 
